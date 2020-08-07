8e98ff2b563a55b50c6ae8c9c5f64952a939eb11
afreakk <laderud@hotmail.com>
2020-08-07 23:11:25 +0200

    isolate flexible context to the one thing that needs it

--- xmonad/Main.hs
+++ xmonad/Main.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE FlexibleContexts #-}
 import XMonad as XM
 import System.Exit
 import XMonad.Hooks.DynamicLog
@@ -14,9 +13,7 @@ import XMonad.Prompt
 import XMonad.Layout.WorkspaceDir
 import XMonad.Actions.CopyWindow
 import XMonad.Layout.SubLayouts
-import XMonad.Layout.Simplest
 import XMonad.Hooks.WorkspaceHistory (workspaceHistoryHook)
-import XMonad.Layout.WindowNavigation
 
 import AConfig (getConfig, AConfig (..))
 import XmobarUtils (xmobarShorten)
@@ -26,6 +23,7 @@ import qualified Data.Map        as M
 import Calculator (calculatorPrompt)
 import BackAndForth (backAndForth)
 import ExtraKeyCodes
+import LayoutHook (myLayout)
 
 -- Prompt theme
 myXPConfig :: AConfig -> XPConfig
@@ -187,30 +185,6 @@ myTabConfig cfg = def
   , fontName = cl_font cfg
   , decoHeight = fromIntegral $ cl_barHeight cfg
   }
-
-------------------------------------------------------------------------
--- Layouts:
--- You can specify and transform your layouts by modifying these values.
--- If you change layout bindings be sure to use 'mod-shift-space' after
--- restarting (with 'mod-q') to reset your layout state to the new
--- defaults, as xmonad preserves your old layout settings by default.
---
--- The available layouts.  Note that each layout is separated by |||,
--- which denotes layout choice.
---
-myLayout tabcfg = tabbed ||| normalTiled ||| bottomTiled
-  where
-    tabbed      = tabbedBottom shrinkText tabcfg
-    bottomTiled = (addTabsBottom shrinkText tabcfg (subLayout [] Simplest (Mirror tiled)))
-    normalTiled = (addTabsBottom shrinkText tabcfg (subLayout [] Simplest tiled))
-    -- default tiling algorithm partitions the screen into two panes
-    tiled   = Tall nmaster delta ratio
-    -- The default number of windows in the master pane
-    nmaster = 1
-    -- Default proportion of screen occupied by master pane
-    ratio   = 1/2
-    -- Percent of screen to increment by when resizing panes
-    delta   = 3/100
 
 ------------------------------------------------------------------------
 -- Window rules:
